5a1716b154d9c4eb569d55564d6c3aa25f24459b
SANTIAGO CARMONA MONTOYA <santiagocarmona224871@correo.itm.edu.co>
2022-04-13 18:01:33 -0500

    Finish States CRUD

--- Cake-DessertShop/Cake-DessertShop/Controllers/UbicationsController.cs
+++ Cake-DessertShop/Cake-DessertShop/Controllers/UbicationsController.cs
@@ -15,13 +15,13 @@ namespace CakeDessertShop.Controllers
             _context = context;
         }
 
-        // GET: Ubications
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await _context.States.ToListAsync());
         }
 
-        // GET: Ubications/Details/5
+        [HttpGet]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -29,7 +29,7 @@ namespace CakeDessertShop.Controllers
                 return NotFound();
             }
 
-            var state = await _context.States
+            State state = await _context.States
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (state == null)
             {
@@ -39,29 +39,45 @@ namespace CakeDessertShop.Controllers
             return View(state);
         }
 
-        // GET: Ubications/Create
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Ubications/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] State state)
+        public async Task<IActionResult> Create(State state)
         {
             if (ModelState.IsValid)
             {
                 _context.Add(state);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
+                try
+                {
+                    await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
+                }
+                catch (DbUpdateException dbUpdateException)
+                {
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                    {
+                        ModelState.AddModelError(string.Empty, "Ya existe un departamento con el mismo nombre.");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
+                    }
+                }
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
             }
             return View(state);
         }
 
-        // GET: Ubications/Edit/5
+
+        [HttpGet]
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -77,12 +93,9 @@ namespace CakeDessertShop.Controllers
             return View(state);
         }
 
-        // POST: Ubications/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] State state)
+        public async Task<IActionResult> Edit(int id, State state)
         {
             if (id != state.Id)
             {
@@ -91,28 +104,32 @@ namespace CakeDessertShop.Controllers
 
             if (ModelState.IsValid)
             {
+                _context.Update(state);
                 try
                 {
-                    _context.Update(state);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
-                catch (DbUpdateConcurrencyException)
+                catch (DbUpdateException dbUpdateException)
                 {
-                    if (!StateExists(state.Id))
+                    if (dbUpdateException.InnerException.Message.Contains("duplicate"))
                     {
-                        return NotFound();
+                        ModelState.AddModelError(string.Empty, "Ya existe un departamento con el mismo nombre.");
                     }
                     else
                     {
-                        throw;
+                        ModelState.AddModelError(string.Empty, dbUpdateException.InnerException.Message);
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                catch (Exception exception)
+                {
+                    ModelState.AddModelError(string.Empty, exception.Message);
+                }
             }
             return View(state);
         }
 
-        // GET: Ubications/Delete/5
+        [HttpGet]
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -120,7 +137,7 @@ namespace CakeDessertShop.Controllers
                 return NotFound();
             }
 
-            var state = await _context.States
+            State state = await _context.States
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (state == null)
             {
@@ -130,20 +147,14 @@ namespace CakeDessertShop.Controllers
             return View(state);
         }
 
-        // POST: Ubications/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> DeleteConfirmed(int id)
         {
-            var state = await _context.States.FindAsync(id);
+            State state = await _context.States.FindAsync(id);
             _context.States.Remove(state);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
-        }
-
-        private bool StateExists(int id)
-        {
-            return _context.States.Any(e => e.Id == id);
         }
     }
 }
--- Cake-DessertShop/Cake-DessertShop/Program.cs
+++ Cake-DessertShop/Cake-DessertShop/Program.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<DataContext>(o =>
 {
     o.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
