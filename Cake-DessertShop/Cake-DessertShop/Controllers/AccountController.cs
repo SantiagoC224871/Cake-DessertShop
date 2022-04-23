92359eb60b7c115fb34759390de05fd73eb98050
JUAN MANUEL AGUDELO OLARTE <juanma112830@gmail.com>
2022-04-23 18:48:37 -0500

    Add admins, users and change password

--- Cake-DessertShop/Cake-DessertShop/Controllers/AccountController.cs
+++ Cake-DessertShop/Cake-DessertShop/Controllers/AccountController.cs
@@ -29,7 +29,7 @@ namespace CakeDessertShop.Controllers
             AddUserViewModel model = new()
             {
                 Id = Guid.Empty.ToString(),
-                States = await _combosHelper.GetComboStatesAsync(),
+                States = await _combosHelper.GetComboStatesAsync(),                
                 Cities = await _combosHelper.GetComboCitiesAsync(0),
                 Neighborhoods = await _combosHelper.GetComboNeighborhoodsAsync(0),
                 UserType = UserType.User,
@@ -50,15 +50,19 @@ namespace CakeDessertShop.Controllers
                 {
                     imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
                 }
+
                 model.ImageId = imageId;
                 User user = await _UserHelper.AddUserAsync(model);
                 if (user == null)
                 {
                     ModelState.AddModelError(string.Empty, "Este correo ya está siendo usado.");
+                    model.States = await _combosHelper.GetComboStatesAsync();
+                    model.Cities = await _combosHelper.GetComboCitiesAsync(model.StateId);
+                    model.Neighborhoods = await _combosHelper.GetComboNeighborhoodsAsync(model.CityId);
                     return View(model);
                 }
 
-                LoginViewModel loginViewModel = new LoginViewModel
+                LoginViewModel loginViewModel = new()
                 {
                     Password = model.Password,
                     RememberMe = false,
@@ -73,6 +77,9 @@ namespace CakeDessertShop.Controllers
                 }
             }
 
+            model.States = await _combosHelper.GetComboStatesAsync();
+            model.Cities = await _combosHelper.GetComboCitiesAsync(model.StateId);
+            model.Neighborhoods = await _combosHelper.GetComboNeighborhoodsAsync(model.CityId);
             return View(model);
         }
 
@@ -140,6 +147,104 @@ namespace CakeDessertShop.Controllers
             }
 
             return Json(city.Neighborhoods.OrderBy(c => c.Name));
+        }
+
+        public async Task<IActionResult> ChangeUser()
+        {
+            User user = await _UserHelper.GetUserAsync(User.Identity.Name);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            EditUserViewModel model = new()
+            {
+                Address = user.Address,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                PhoneNumber = user.PhoneNumber,
+                ImageId = user.ImageId,
+                Neighborhoods = await _combosHelper.GetComboNeighborhoodsAsync(user.Neighborhood.City.Id),
+                NeighborhoodId = user.Neighborhood.Id,
+                States = await _combosHelper.GetComboStatesAsync(),
+                StateId = user.Neighborhood.City.State.Id,
+                CityId = user.Neighborhood.City.Id,
+                Cities = await _combosHelper.GetComboCitiesAsync(user.Neighborhood.City.State.Id),
+                Id = user.Id,
+                Document = user.Document
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ChangeUser(EditUserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = model.ImageId;
+
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                User user = await _UserHelper.GetUserAsync(User.Identity.Name);
+
+                user.FirstName = model.FirstName;
+                user.LastName = model.LastName;
+                user.Address = model.Address;
+                user.PhoneNumber = model.PhoneNumber;
+                user.ImageId = imageId;
+                user.Neighborhood = await _context.Neighborhoods.FindAsync(model.NeighborhoodId);
+                user.Document = model.Document;
+
+                await _UserHelper.UpdateUserAsync(user);
+                return RedirectToAction("Index", "Home");
+            }
+            model.States = await _combosHelper.GetComboStatesAsync();
+            model.Cities = await _combosHelper.GetComboCitiesAsync(model.StateId);
+            model.Neighborhoods = await _combosHelper.GetComboNeighborhoodsAsync(model.CityId);
+            return View(model);
+        }
+
+        public IActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                if (model.OldPassword == model.NewPassword)
+                {
+                    ModelState.AddModelError(String.Empty, "Ingresar una contraseña diferente");
+                    return View(model);
+                }
+
+                var user = await _UserHelper.GetUserAsync(User.Identity.Name);
+                if (user != null)
+                {
+                    var result = await _UserHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return RedirectToAction("ChangeUser");
+                    }
+                    else
+                    {
+                        ModelState.AddModelError(string.Empty, result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError(string.Empty, "Usuario no encontrado.");
+                }
+            }
+
+            return View(model);
         }
 
 
