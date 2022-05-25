697b9393479c57b475345adef6f8b9b6da951071
SANTIAGO CARMONA MONTOYA <santiagocarmona224871@correo.itm.edu.co>
2022-05-25 18:00:17 -0500

    CSS buttons update, fonts

--- Cake-DessertShop/Cake-DessertShop/Models/EditProductViewModel.cs
+++ Cake-DessertShop/Cake-DessertShop/Models/EditProductViewModel.cs
@@ -7,7 +7,7 @@ namespace CakeDessertShop.Models
         public int Id { get; set; }
 
         [Display(Name = "Nombre")]
-        [MaxLength(50, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
+        [MaxLength(24, ErrorMessage = "El campo {0} debe tener máximo {1} caractéres.")]
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Name { get; set; }
 
